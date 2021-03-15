7e87b28ea9e7ca4ecaba5d6d7806b17bb43e3786
tqla122-19-4 <ssova228@gmail.com>
2021-03-15 19:51:40 +0200

    lab / java bigdecimal calculator

--- src/com/company/Calculate.java
+++ src/com/company/Calculate.java
@@ -1,11 +1,10 @@
 package com.company;
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 public class Calculate {
     public static double calcExactValue(double x)
     {
-        return  Math.cos(x);
+        return  (1/2-Math.PI/4*Math.abs(Math.sin(x)));
 
     }
     public static double calcApproxValue(double x, int n)
@@ -13,25 +12,24 @@ public class Calculate {
         double Sn = 0;
         for (int i = 1; i<=n; i++)
         {
-            Sn += Math.pow(-1, i) * (Math.pow(x,2*i)/2*i);
+            Sn +=Math.cos(x*2*i)/(4*Math.pow(i,2)-1);
         }
         return Sn;
     }
     public static double calcApproxValue(double x, double e)
     {
-
         double Se = 0;
         int i = 1;
         while (Math.abs(Se) <= e)
         {
-            Se += Math.pow(-1, i) * (Math.pow(x,2*i)/2*i);
+            Se += Math.cos(x*2*i)/(4*Math.pow(i,2)-1);
         }
         return Se;
     }
 
     public static BigDecimal calcExactValue(BigDecimal x)
     {
-        BigDecimal y = BigDecimal.valueOf(Math.cos(x.doubleValue()));
+        BigDecimal y = BigDecimal.valueOf(1/2-Math.PI/4*Math.abs(Math.sin(x.doubleValue())));
         return y;
     }
 
@@ -40,9 +38,7 @@ public class Calculate {
         BigDecimal Sn = BigDecimal.valueOf(0);
         for (int i = 1; i<=n; i++)
         {
-            BigDecimal pow_1 = BigDecimal.valueOf(-1);
-            pow_1 = pow_1.pow(i);
-            Sn = Sn.add(x.pow(2*i).divide(BigDecimal.valueOf(2*i), 100, RoundingMode.CEILING).multiply(pow_1));
+            Sn = Sn.add(BigDecimal.valueOf(Math.cos(2*x.doubleValue())/(4*Math.pow(i,2)-1)));
         }
         return Sn;
     }
@@ -53,9 +49,7 @@ public class Calculate {
         int i = 1;
         while (Se.abs().signum() <= e)
         {
-            BigDecimal pow_1 = BigDecimal.valueOf(-1);
-            pow_1 = pow_1.pow(i);
-            Se = Se.add(x.pow(2*i).divide(BigDecimal.valueOf(2*i), 100, RoundingMode.CEILING).multiply(pow_1));
+            Se = Se.add(BigDecimal.valueOf(Math.cos(2*x.doubleValue())/(4*Math.pow(i,2)-1)));
         }
         return Se;
     }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,29 +11,29 @@ public class Main
         double x, k, a, b, y, e, E, ApproxValue;
         int n;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Хотите использовать следующие данны?");
-        System.out.println(" Нижняя граница а = 0,1 \n Верхня граница b = 1 \n Количество шагов k = 10 \n Количество элементов ряда n = 10 \n Допустимая погрешность e = 0,0001");
-        System.out.println("Ввести с клавиатуры новые? \ny/n");
-        String input = scanner.nextLine();
-        if (input.equals('y'))
+        System.out.println("Would you like to use standard data?\n1 - enter new data from the keyboard\n2 - to use this data:\nBottom bound а = 0,1 \nUpper bound b = 0.8\nNumber of steps k = 10\nNumber of elements in a row n = 50\nAllowable error e = 0,0001 ");
+        int choice = scanner.nextInt();
+        if (choice==1)
         {
-            System.out.println("Введите нижнюю границу");
+            System.out.println("Enter the Bottom bound:");
             a = scanner.nextDouble();
-            System.out.println("Введите верхнюю границу");
+            System.out.println("Enter the Upper bound:");
             b = scanner.nextDouble();
-            System.out.println("Введите количество шагов");
+            System.out.println("Enter the Number of steps:");
             k = scanner.nextDouble();
-            System.out.println("Введите количество элементов ряда n");
+            System.out.println("Enter the Number of elements in a row:");
             n = scanner.nextInt();
-            System.out.println("Введите допустимую погрешность");
+            System.out.println("Enter the Allowable error:");
             e = scanner.nextDouble();
+
+
         }
         else
         {
             a = 0.1;
-            b = 1;
+            b = 0.8;
             k = 10;
-            n = 10;
+            n = 50;
             e = 0.0001;
         }
         BigDecimal DecimalX, DecimalK, DecimalA, DecimalB, DecimalY, DecimalE, DecimalApprox;
@@ -42,7 +42,7 @@ public class Main
         DecimalK = BigDecimal.valueOf(k);
         BigDecimal DecimalStep = DecimalB.subtract(DecimalA).divide(DecimalK,100,   RoundingMode.CEILING);
         double step = (b - a) / k;
-        System.out.println("Тип данных double:");
+        System.out.println("double:");
         for (x = a; x <= b; x += step) {
             System.out.println("x = " + x);
             y = Calculate.calcExactValue(x);
@@ -50,13 +50,13 @@ public class Main
             ApproxValue = Calculate.calcApproxValue(x, n);
             System.out.println("Sn = " + ApproxValue);
             E = (y - ApproxValue) / y;
-            System.out.println("Относительная погрешность = " + E);
+            System.out.println("Allowable error = " + E);
             ApproxValue = Calculate.calcApproxValue(x, e);
             System.out.println("Se = " + ApproxValue);
             E = (y - ApproxValue) / y;
-            System.out.println("Относительная погрешность = " + E);
+            System.out.println("Allowable error = " + E);
         }
-        System.out.println("Тип данных BigDecimal:");
+        System.out.println("BigDecimal:");
         for (DecimalX=DecimalA; DecimalX.compareTo(DecimalB) != 0; DecimalX = DecimalX.add(DecimalStep))
         {
             System.out.println("x = " + DecimalX);
@@ -65,11 +65,11 @@ public class Main
             DecimalApprox = Calculate.calcApproxValue(DecimalX, n);
             System.out.println("Sn = " + DecimalApprox);
             DecimalE = DecimalY.subtract(DecimalApprox).divide(DecimalY, 100, RoundingMode.CEILING);
-            System.out.println("Относительная погрешность = " + DecimalE);
+            System.out.println("Allowable error = " + DecimalE);
             DecimalApprox = Calculate.calcApproxValue(DecimalX, e);
             System.out.println("Se = " + DecimalApprox);
             DecimalE = DecimalY.subtract(DecimalApprox).divide(DecimalY, 100,  RoundingMode.CEILING);
-            System.out.println("Относительная погрешность = " + DecimalE);
+            System.out.println("Allowable error = " + DecimalE);
         }
     }
 }
